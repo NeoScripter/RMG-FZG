4453a47ee14e568c9aad0e21003314a316c28b49
Ilya <sange0337@gmail.com>
2024-08-08 13:07:00 +0800

    finished the banner on the homepage

--- footer.php
+++ footer.php
@@ -1,7 +1,16 @@
 <footer class="footer">
-    
+
 </footer>
 
-<?php wp_footer();?>
+<?php wp_footer(); ?>
+<style>
+    .home-banner {
+        background-image: url(<?php echo get_template_directory_uri() . "/assets/imgs/purple-planet.webp"; ?>);
+    }
+    .home-banner__title {
+        background-image: url(<?php echo get_template_directory_uri() . "/assets/svgs/bg-title.svg"; ?>);
+    }
+</style>
 </body>
+
 </html>
--- front-page.php
+++ front-page.php
@@ -1,7 +1,41 @@
 <?php get_header(); ?>
-Hello World!
-
 <div class="wrapper">
-    It's a wrapper
+    <section class="home-banner">
+        <div class="home-banner__title">
+            <h1 class="home-banner__heading">Welcome to RMG FZC</h1>
+            <img src="<?php echo get_template_directory_uri() . "/assets/svgs/preview.svg"; ?>" alt="white dot with a horizontal line on the right">
+        </div>
+        <div class="home-banner__content">
+            <p>Your Trusted Gold Bullion Dealer in Hong Kong SAR, China</p>
+            <p>Providing investors around the world with seamless access to professional bullion markets.</p>
+            <ul class="home-banner__actions">
+                <li><a href="">sell gold</a></li>
+                <li><a href="">buy gold</a></li>
+                <li><a href="">store gold</a></li>
+                <li><a href="">create account</a></li>
+            </ul>
+        </div>
+    </section>
+    <section class="home-intro">
+        <h2>Introduction</h2>
+        <p>At RMG FZC, <span class="text-golden">we provide investors around the world with seamless access to the professional bullion markets.</span> Whether you're looking to buy, sell, or store gold bullion, our services are designed to meet your needs with efficiency and reliability.</p>
+        <div class="home-intro__items">
+            <div class="home-intro__item">
+                <div>
+                    <h3>Secure & Accessible Storage</h3>
+                    <p>Our professional-market vaults in Hong Kong are equipped with state-of-the-art security measures, ensuring your gold is stored safely</p>
+                    <p>You can rest assured that your investments are in good hands. Additionally, our storage solutions are designed to be easily accessible, giving you peace of mind and full control over your assets.</p>
+                    <a href="">Buy Gold</a>
+                </div>
+                <img src="<?php echo get_template_directory_uri() . "/assets/imgs/home-1.png"; ?>" alt="">
+            </div>
+            <div class="home-intro__item">
+
+            </div>
+            <div class="home-intro__item">
+
+            </div>
+        </div>
+    </section>
 </div>
 <?php get_footer(); ?>
