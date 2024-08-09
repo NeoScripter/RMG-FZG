4e485528014d36f2f68b1f4f616db25ff12a60f5
Ilya <sange0337@gmail.com>
2024-08-09 15:20:19 +0800

    finished the storage section

--- templates/template-storage.php
+++ templates/template-storage.php
@@ -15,15 +15,85 @@
                 <p class="storage-intro__item-description">At RMG FZC, we understand the importance of securely storing your valuable investments. Our storage solutions are designed to offer the highest level of security and accessibility, ensuring that your gold bullion is protected at all times.</p>
                 <a href="" class="storage-intro__item-link">Get Started</a>
             </div>
-            <img class="storage-intro__item-image" src="<?php echo get_template_directory_uri() . "/assets/imgs/storage-1.png"; ?>" alt="Golden spiral object">
+            <img class="storage-intro__item-image" src="<?php echo get_template_directory_uri() . "/assets/imgs/storage-1.png"; ?>" alt="Golden object resembling is seat">
         </div>
         <div class="storage-intro__item">
-            <img class="storage-intro__item-image" src="<?php echo get_template_directory_uri() . "/assets/imgs/storage-2.png"; ?>" alt="Golden dragon">
+            <img class="storage-intro__item-image" src="<?php echo get_template_directory_uri() . "/assets/imgs/storage-2.png"; ?>" alt="Golden piece of chain">
             <div class="storage-intro__item-content">
                 <h3 class="storage-intro__item-heading">State-of-the-Art Security</h3>
                 <p class="storage-intro__item-description">Our gold bullion is stored in professional-market vaults in Hong Kong, equipped with the latest security technologies. These vaults are monitored 24/7, ensuring your assets are safeguarded against any potential threats.</p>
                 <a href="" class="storage-intro__item-link">Get Started</a>
             </div>
+        </div>
+    </section>
+    <section class="storage-main__items">
+        <div class="storage-main__item">
+            <div class="storage-main__item-content">
+                <h3 class="storage-main__item-heading">Security Features</h3>
+                <p class="storage-main__item-description">The vaults we use are trusted by leading financial institutions and bullion markets around the world. These facilities meet the highest industry standards for security and integrity, providing you with peace of mind that your investments are in good hands.</p>
+                <div class="storage-main__item__flex-group">
+                    <div class="storage-main__item__flex-group-item">24/7 surveillance and monitoring</div>
+                    <div class="storage-main__item__flex-group-item">Professional-Market Vaults</div>
+                    <div class="storage-main__item__flex-group-item">Advanced alarm systems</div>
+                    <div class="storage-main__item__flex-group-item">Trusted by Industry Experts</div>
+                    <div class="storage-main__item__flex-group-item">Controlled access protocols</div>
+                </div>
+            </div>
+            <img class="storage-main__item-image" src="<?php echo get_template_directory_uri() . "/assets/imgs/storage-3.png"; ?>" alt="Golden croissant">
+        </div>
+        <div class="storage-main__item">
+            <img class="storage-main__item-image" src="<?php echo get_template_directory_uri() . "/assets/imgs/storage-4.png"; ?>" alt="Golden star">
+            <div class="storage-main__item-content">
+                <h3 class="storage-main__item-heading">Vault Benefits</h3>
+                <p class="storage-main__item-description">We include comprehensive insurance in our storage solutions, covering your gold bullion against a wide range of risks. This insurance ensures that your investments are protected, offering you additional security and confidence.</p>
+                <div class="storage-main__item__flex-group">
+                    <div class="storage-main__item__flex-group-item">High industry standards</div>
+                    <div class="storage-main__item__flex-group-item">Trusted by professionals</div>
+                    <div class="storage-main__item__flex-group-item">Comprehensive Insurance</div>
+                    <div class="storage-main__item__flex-group-item">Protection Against All Risks</div>
+                    <div class="storage-main__item__flex-group-item">Robust security measures</div>
+                </div>
+            </div>
+        </div>
+        <div class="storage-main__item">
+            <div class="storage-main__item-content">
+                <h3 class="storage-main__item-heading">Accessibility Features</h3>
+                <p class="storage-main__item-description">Our team of professionals is highly experienced in managing and handling gold bullion. We follow strict protocols to ensure that your investments are treated with the utmost care and professionalism, from storage to transportation.</p>
+                <div class="storage-main__item__flex-group">
+                    <div class="storage-main__item__flex-group-item">Simple withdrawal process</div>
+                    <div class="storage-main__item__flex-group-item">Immediate liquidity options</div>
+                    <div class="storage-main__item__flex-group-item">Professional Handling</div>
+                    <div class="storage-main__item__flex-group-item">Expertise in Bullion Management</div>
+                    <div class="storage-main__item__flex-group-item">Secure handling of assets</div>
+                </div>
+            </div>
+            <img class="storage-main__item-image" src="<?php echo get_template_directory_uri() . "/assets/imgs/storage-5.png"; ?>" alt="Golden ring">
+        </div>
+        <div class="storage-main__item">
+            <img class="storage-main__item-image" src="<?php echo get_template_directory_uri() . "/assets/imgs/storage-6.png"; ?>" alt="Golden cone">
+            <div class="storage-main__item-content">
+                <h3 class="storage-main__item-heading">Insurance Coverage</h3>
+                <p class="storage-main__item-description">While security is our top priority, we also ensure that your gold bullion is easily accessible when you need it. Our storage solutions are designed to provide you with full control over your assets, allowing for quick and easy withdrawals without compromising on security.</p>
+                <div class="storage-main__item__flex-group">
+                    <div class="storage-main__item__flex-group-item">Full Control Over Your Assets</div>
+                    <div class="storage-main__item__flex-group-item">Theft and damage</div>
+                    <div class="storage-main__item__flex-group-item">Other unforeseen events</div>
+                    <div class="storage-main__item__flex-group-item">Easy Accessibility</div>
+                </div>
+            </div>
+        </div>
+        <div class="storage-main__item">
+            <div class="storage-main__item-content">
+                <h3 class="storage-main__item-heading">Handling Protocols</h3>
+                <p class="storage-main__item-description">Experience the benefits of investing in gold bullion with RMG FZC. Our commitment to expertise, professionalism, and exceptional service makes us the ideal choice for investors around the world. Start your journey with us today and take advantage of the professional bullion markets with ease.</p>
+                <div class="storage-main__item__flex-group">
+                    <div class="storage-main__item__flex-group-item">Strict handling procedures</div>
+                    <div class="storage-main__item__flex-group-item">Join RMG FZC Today</div>
+                    <div class="storage-main__item__flex-group-item">Secure transportation</div>
+                    <div class="storage-main__item__flex-group-item">Professional management</div>
+                </div>
+            </div>
+            <img class="storage-main__item-image" src="<?php echo get_template_directory_uri() . "/assets/imgs/storage-7.png"; ?>" alt="Golden piramide">
         </div>
     </section>
 </div>
