2cedcee434a3bc567320f1657a754739aee65e83
Ilya <sange0337@gmail.com>
2024-08-09 22:42:01 +0800

    finished the legal section

--- templates/template-legal.php
+++ templates/template-legal.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template Name: Legal and Compliance
  */
@@ -24,9 +25,104 @@
             </div>
         </div>
     </section>
-    <section class="legal-banner">
-        <h1 class="legal-banner__heading">Our AML Measures Include:</h1>
-        <p class="legal-banner__description">For more information on AML regulations, please visit the <span class="text-golden">Hong Kong Customs and Excise Department website.</span> Compliance with Precious Metals Regulations As a dealer in precious metals, RMG FZC complies with all relevant regulations governing the trade and storage of gold bullion. This ensures that our operations are transparent, legal, and secure.</p>
+    <section class="legal-main">
+        <div class="legal-main__item">
+            <div class="legal-main__item-banner">
+                <h3 class="legal-main__item-heading">Our AML Measures Include:</h3>
+                <p class="legal-main__item-description">For more information on AML regulations, please visit the <span class="text-golden">Hong Kong Customs and Excise Department website.</span> Compliance with Precious Metals Regulations As a dealer in precious metals, RMG FZC complies with all relevant regulations governing the trade and storage of gold bullion. This ensures that our operations are transparent, legal, and secure.</p>
+            </div>
+            <div class="legal-main__item-content">
+                <div class="legal-main__item-list">
+                    <div class="legal-main__item-list__li">
+                        <h4>Customer Due Diligence (CDD)</h4>
+                        <p>We perform thorough identity verification and background checks on all customers.</p>
+                    </div>
+                    <div class="legal-main__item-list__li">
+                        <h4>Record Keeping</h4>
+                        <p>We maintain comprehensive records of all transactions for a minimum period as required by law.</p>
+                    </div>
+                    <div class="legal-main__item-list__li">
+                        <h4>Suspicious Transaction Reporting</h4>
+                        <p>We promptly report any suspicious transactions to the appropriate authorities.</p>
+                    </div>
+                    <div class="legal-main__item-list__li">
+                        <h4>Employee Training</h4>
+                        <p>Our staff undergoes regular training to stay updated on AML laws and practices.</p>
+                    </div>
+                </div>
+                <img class="legal-main__item-image" src="<?php echo get_template_directory_uri() . "/assets/imgs/legal-3.png"; ?>" alt="Blue planet">
+            </div>
+        </div>
+        <div class="legal-main__item">
+            <div class="legal-main__item-banner">
+                <h3 class="legal-main__item-heading">Key Compliance Areas</h3>
+                <p class="legal-main__item-description">We are committed to protecting your privacy and personal data. Our data protection policies are designed to safeguard your information and ensure it is used in compliance with applicable laws.
+                </p>
+            </div>
+            <div class="legal-main__item-content">
+                <img class="legal-main__item-image" src="<?php echo get_template_directory_uri() . "/assets/imgs/legal-4.png"; ?>" alt="Orange planet">
+                <div class="legal-main__item-list">
+                    <div class="legal-main__item-list__li">
+                        <h4>Customer Due Diligence (CDD)</h4>
+                        <p>We perform thorough identity verification and background checks on all customers.</p>
+                    </div>
+                    <div class="legal-main__item-list__li">
+                        <h4>Record Keeping</h4>
+                        <p>We maintain comprehensive records of all transactions for a minimum period as required by law.</p>
+                    </div>
+                    <div class="legal-main__item-list__li">
+                        <h4>Suspicious Transaction Reporting</h4>
+                        <p>We promptly report any suspicious transactions to the appropriate authorities.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="legal-main__item">
+            <div class="legal-main__item-banner">
+                <h3 class="legal-main__item-heading">Our AML Measures Include:</h3>
+                <p class="legal-main__item-description">For more information on AML regulations, please visit the <span class="text-golden">Hong Kong Customs and Excise Department website.</span> Compliance with Precious Metals Regulations As a dealer in precious metals, RMG FZC complies with all relevant regulations governing the trade and storage of gold bullion. This ensures that our operations are transparent, legal, and secure.</p>
+            </div>
+            <div class="legal-main__item-content">
+                <div class="legal-main__item-list">
+                    <div class="legal-main__item-list__li">
+                        <h4>Customer Due Diligence (CDD)</h4>
+                        <p>We perform thorough identity verification and background checks on all customers.</p>
+                    </div>
+                    <div class="legal-main__item-list__li">
+                        <h4>Record Keeping</h4>
+                        <p>We maintain comprehensive records of all transactions for a minimum period as required by law.</p>
+                    </div>
+                    <div class="legal-main__item-list__li">
+                        <h4>Suspicious Transaction Reporting</h4>
+                        <p>We promptly report any suspicious transactions to the appropriate authorities.</p>
+                    </div>
+                </div>
+                <img class="legal-main__item-image" src="<?php echo get_template_directory_uri() . "/assets/imgs/legal-5.png"; ?>" alt="Blue planet">
+            </div>
+        </div>
+    </section>
+    <section class="legal-docs">
+        <div class="legal-docs__item">
+            <div class="legal-docs__item-content">
+                <h3 class="legal-docs__item-heading">Responsible Gold Supply Chain Policy</h3>
+                <p class="legal-docs__item-description">Outlines RMG FZC's commitment to sourcing gold responsibly, ensuring ethical practices, environmental sustainability, and respect for human rights throughout the supply chain.</p>
+            </div>
+            <a href="<?php echo get_template_directory_uri() . "/assets/docs/aml-policy.pdf"; ?>" class="legal-docs__item-link" download>DOWNLOAD PDF</a>
+        </div>
+        <div class="legal-docs__item">
+            <div class="legal-docs__item-content">
+                <h3 class="legal-docs__item-heading">Anti-Bribery and Anti-Corruption Policy</h3>
+                <p class="legal-docs__item-description">At RMG FZC, we adhere to the highest standards of legal and regulatory compliance. We are committed to maintaining the integrity of our operations and ensuring the safety and legality of your investments.</p>
+            </div>
+            <a href="<?php echo get_template_directory_uri() . "/assets/docs/anti-bribery.pdf"; ?>" class="legal-docs__item-link" download>DOWNLOAD PDF</a>
+        </div>
+        <div class="legal-docs__item">
+            <div class="legal-docs__item-content">
+                <h3 class="legal-docs__item-heading">Anti-Money Laundering & Counter Financing of Terrorism Policy</h3>
+                <p class="legal-docs__item-description">Describes RMG FZC's procedures to prevent money laundering and terrorist financing, ensuring compliance with relevant laws and regulations to protect our business and stakeholders from financial crime.</p>
+            </div>
+            <a href="<?php echo get_template_directory_uri() . "/assets/docs/cjain-policy.pdf"; ?>" class="legal-docs__item-link" download>DOWNLOAD PDF</a>
+        </div>
     </section>
 </div>
 <?php get_footer(); ?>
